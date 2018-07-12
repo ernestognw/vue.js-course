50bb5249519706b38be6cebf0f4c0fb0ce514fc6
ernestognw <ernestognw@gmail.com>
2018-07-12 13:35:51 -0500

    first commit

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	PlatziMusic
+// API key	3bb943e7609b01a5848ceb5d570d0393
+// Shared secret	b070a31c0ea537a79529057ec35c6c08
+// Registered to	ernestognw
+
+export default {
+  appname: 'platzimusic',
+  apiKey: '3bb943e7609b01a5848ceb5d570d0393',
+  secret: 'b070a31c0ea537a79529057ec35c6c08',
+  registeredTo: 'ernestognw'
+}
